8098fde31aecea2e9b7ff9f09bd0788f19a43b83
user <davidtr1037@gmail.com>
2020-04-23 19:54:28 +0300

    updating

--- m4/run_overhead.sh
+++ m4/run_overhead.sh
@@ -4,5 +4,5 @@ CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/run.sh
 
 MAX_INST=1231677
-run_klee_2
-run_memory_model "-search=dfs"
+OUT_DIR=${OVERHEAD_DIR}/out-klee-m4 run_klee_2
+OUT_DIR=${OVERHEAD_DIR}/out-mm-m4 run_memory_model "-search=dfs"
