d1bc9eb7186de359588c112d46c309f71821c789
user <davidtr1037@gmail.com>
2020-04-23 17:30:11 +0300

    updating

--- parse_merge.py
+++ parse_merge.py
@@ -21,13 +21,12 @@ DIRS = [
     "out-dsmm-default",
 ]
 
-def dump_queries(program_dir):
+def dump(program_dir):
     for d in DIRS:
         try:
             klee_out = KLEEOut(os.path.join(program_dir, d), open_stats=False)
             print "  {}: {}".format(d, klee_out.elapsed)
-        except Exception as e:
-            print e
+        except:
             print "failed {}".format(d)
 
 def main():
@@ -39,7 +38,7 @@ def main():
     overhead = []
     for p in PROGRAMS:
         print "program: {}".format(p)
-        dump_queries(os.path.join(results_dir, p))
+        dump(os.path.join(results_dir, p))
 
 if __name__ == "__main__":
     main()
