c5b2aabaaaa6f43eed4e73b5b8c22ccf921d62c4
user <davidtr1037@gmail.com>
2020-05-20 18:13:57 +0300

    updating limit

--- binutils/run_overhead.sh
+++ binutils/run_overhead.sh
@@ -3,6 +3,6 @@
 CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
 source ${CURRENT_DIR}/run.sh
 
-MAX_INST=43430116
+MAX_INST=43430065
 OUT_DIR=${OVERHEAD_DIR}/out-klee-gas run_klee "-search=dfs"
 OUT_DIR=${OVERHEAD_DIR}/out-mm-gas run_memory_model "-search=dfs"
